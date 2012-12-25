a5bee26d832555e03282828b1343ed5130e44e0e
mat kelcey <matthew.kelcey@gmail.com>
2012-12-24 22:49:24 -0800

    wip

---
+++ exercises/s07.4.1.r
@@ -0,0 +1,14 @@
+# s7.4.1
+
+library(rjags)
+
+flips = c(rep(1,11), rep(0,3))
+model = jags.model('s7.4.1.model', 
+                   data=list(nFlips=length(flips), y=flips))
+
+update(model, 2000)
+
+s = jags.samples(model, c('theta'), 100)
+
+s$theta
+# 0.76
